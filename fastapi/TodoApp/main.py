1f71b029280fa81fa52fb5cfc34455926ecd562f
yuuio3 <152229366+yuuio3@users.noreply.github.com>
2024-03-13 08:50:57 +0000

    ok

--- fastapi/TodoApp/main.py
+++ fastapi/TodoApp/main.py
@@ -1,8 +1,9 @@
 from typing import Annotated
 from sqlalchemy.orm import Session
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, HTTPException, Depends, Path
 import models
 from models import Todos
+from starlette import status
 
 from database import engine, SessionLocal
 
@@ -29,6 +30,14 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@app.get("/")
+@app.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
+
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail="not found")
